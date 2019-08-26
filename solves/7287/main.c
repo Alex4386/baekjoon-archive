48d79ee8722bea5980efa8e0d95e2dace78c6d4f
Alex4386 <psh010209@gmail.com>
2019-08-26 19:41:42 +0900

    add: partial upload - level 01

---
+++ solves/7287/main.c
@@ -0,0 +1,8 @@
+// 정말 ㄹㅇ 복붙하기 전에 잠깐, solved 랑, username 바꿔야 해요.
+#include <stdio.h>
+
+int main() {
+    int solved = 4;
+    char username[9] = "alex4386";
+    printf("%d\n%s",solved,username);
+}
